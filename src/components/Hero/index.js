632355a612346a18e806121ef5ce297f86d7b28d
Auzan <auzan.ip@gmail.com>
2022-03-08 11:08:36 +0700

    add carousel install react slick & slick-carousel install axios

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,64 +1,72 @@
-import React from 'react';
+import React from "react";
+import Carousel from "../Carousel";
+import Data from "../Data";
+import DataMobile from "../Data/mobile";
 
 // CSS
 import "./style.css";
 
 function Hero(props) {
-    
-    return (
-        <div className="col-12 hero-container">
-            <div style={{ backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.57) 87.03%, rgba(255, 255, 255, 0) 138.73%), url(${props.background})` }} className="hero-background col-12"></div>
-            <div className="col-12 hero">
-                <div className="content-container">
-                    <div className="col-xs-12 col-sm-7 col-md-6 col-lg-8 left-side text-light" data-page={props.appName}>
-                        <div className="title-container">
-                            <div className="mb-3 logo">
-                                <img src={props.logo} alt={props.appName} />
-                            </div>
-                            <div className="">
-                                <div className="mb-2 deskripsi">
-                                    <h5>
-                                        {props.deskripsi}
-                                    </h5>
-                                </div>
-                                <div className="mb-2 title">
-                                    <h1>
-                                        {props.title}
-                                    </h1>
-                                </div>
-                            </div>
-                            <div className="slogan">
-                                <p>
-                                    {props.slogan}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 right-side" data-page={props.appName}>
-                        <div className="logo-title mt-5 mb-2">
-                            <div className="d-flex justify-content-center w-100 mb-5">
-                                <div className="logo d-flex justify-content-center col-4">
-                                    <img src={props.logo} alt={props.appName} />
-                                </div>
-                                <div className="d-flex align-items-center deskripsi">
-                                    <h4>
-                                        <b>
-                                            Bantul Projotamansari <br /> Blended Learning
-                                        </b>
-                                    </h4>
-                                </div>
-                            </div>
-                        </div>
-                        {props.children}
-                    </div>
-
+  return (
+    <div className="col-12 hero-container">
+      <div
+        style={{
+          backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.57) 87.03%, rgba(255, 255, 255, 0) 138.73%), url(${props.background})`,
+        }}
+        className="hero-background col-12"
+      ></div>
+      <div className="col-12 hero">
+        <div className="content-container">
+          <div
+            className="col-xs-12 col-sm-7 col-md-6 col-lg-8 left-side text-light"
+            data-page={props.appName}
+          >
+            <div className="title-container">
+              <div className="mb-3 logo">
+                <img src={props.logo} alt={props.appName} />
+              </div>
+              <div className="">
+                <div className="mb-2 deskripsi">
+                  <h5>{props.deskripsi}</h5>
                 </div>
-
+                <div className="mb-2 title">
+                  <h1>{props.title}</h1>
+                </div>
+              </div>
+              <div className="slogan">
+                <p>{props.slogan}</p>
+              </div>
             </div>
-
+          </div>
+          <div
+            className="col-xs-12 col-sm-12 col-md-6 col-lg-4 right-side"
+            data-page={props.appName}
+          >
+            <div className="logo-title mt-5 mb-2">
+              <div className="d-flex justify-content-center w-100 mb-5">
+                <div className="logo d-flex justify-content-center col-4">
+                  <img src={props.logo} alt={props.appName} />
+                </div>
+                <div className="d-flex align-items-center deskripsi">
+                  <h4>
+                    <b>
+                      Bantul Projotamansari <br /> Blended Learning
+                    </b>
+                  </h4>
+                </div>
+              </div>
+            </div>
+            {props.children}
+          </div>
         </div>
-        
-    );
+        <div className="data-container mb-5">
+          <Data />
+          <DataMobile />
+          <Carousel />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/layouts/PageLayout/index.js
+++ src/layouts/PageLayout/index.js
@@ -1,40 +1,37 @@
-
 //NOT EDITABLE!!!
-import Topography from "../../components/Topography"
-import Hero from "../../components/Hero"
-import Footer from "../../components/Footer"
+import Topography from "../../components/Topography";
+import Hero from "../../components/Hero";
+import Footer from "../../components/Footer";
 
 // EDITABLE
-import background from "../../assets/img/background.jpg" //Background
-import logo from '../../assets/img/logo.png'; //Logo
+import background from "../../assets/img/background.jpg"; //Background
+import logo from "../../assets/img/logo.png"; //Logo
 
+function pageLayout(props) {
+  //EDITABLE
+  const deskripsi = "Selamat datang kembali di";
+  const title = "Bantul Projotamansari Blended Learning";
+  const slogan =
+    "Tujuan utama pendidikan bukanlah pengetahuan, tetapi tindakan.";
+  const copyright =
+    "2021 Bantul Projotamansari Blended Learning - Didukung oleh ";
 
-function pageLayout( props ) {
-    //EDITABLE
-    const deskripsi = "Selamat datang kembali di"
-    const title = "Bantul Projotamansari Blended Learning"
-    const slogan = "Tujuan utama pendidikan bukanlah pengetahuan, tetapi tindakan."
-    const copyright = "2021 Bantul Projotamansari Blended Learning - Didukung oleh Geschool"
-
-    return (
-        <div>
-            <Topography />
-            <Hero 
-                logo={logo}
-                background={background}
-                deskripsi={deskripsi}
-                title={title}
-                slogan={slogan}
-                appName={props.appName}
-            >
-                { props.children }
-            </Hero>
-            <Footer
-                copyright={copyright}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Topography />
+      <Hero
+        logo={logo}
+        background={background}
+        deskripsi={deskripsi}
+        title={title}
+        slogan={slogan}
+        appName={props.appName}
+      >
+        {props.children}
+      </Hero>
+      <Footer copyright={copyright} />
+    </div>
+  );
 }
-
 
 export default pageLayout;
